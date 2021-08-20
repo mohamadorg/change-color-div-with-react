8f236e11339aff5577b085ee30e08fe2c6da9f2a
mohamadorg <emohamadweb@gmail.com>
2021-08-20 08:51:56 +0430

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-trfftz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-trfftz)
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const green = '#39D1B4';
+const yellow = '#FFD712';
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { color: green };
+    this.changeColor = this.changeColor.bind(this);
+  }
+  changeColor() {
+    const newColor = this.state.color == green ? yellow : green;
+    this.setState({ color: newColor });
+  }
+  render() {
+    return (
+      <div style={{ background: this.state.color }}>
+        <h1>Change my color</h1>
+        <button onClick={this.changeColor}>Change color</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Toggle />, document.getElementById('root'));
